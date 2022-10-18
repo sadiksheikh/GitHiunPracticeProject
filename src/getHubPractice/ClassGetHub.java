6555e41c205a293777099100670763a5cbf76616
SDK <SDK@DESKTOP-B9KJEIH>
2022-10-18 11:31:44 +0530

    commit code at 10/18/22 11:31 AM

--- src/getHubPractice/ClassGetHub.java
+++ src/getHubPractice/ClassGetHub.java
@@ -5,7 +5,9 @@ public class ClassGetHub
 public static void main(String[] args) {
 
 	 {
-	System.out.println("SadikSheikh No.1 ");	
+	System.out.println("SadikSheikh No.1 ");
+	System.out.println("Javed Bhai Lappas No:1 ");
+	
 	}
 }
 }
